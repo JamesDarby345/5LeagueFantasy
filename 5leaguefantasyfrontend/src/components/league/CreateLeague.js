97bbac7edeb32bbc1bbef0d58c4009230eca3abd
rashyaathar <98115083+rashyaathar@users.noreply.github.com>
2024-03-27 00:30:39 -0400

    Add further changes.

--- 5leaguefantasyfrontend/src/components/league/CreateLeague.js
+++ 5leaguefantasyfrontend/src/components/league/CreateLeague.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import { MDBBtn, MDBContainer, MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function CreateLeague(){
+
+    const API = axios.create({
+        baseURL: 'http://localhost:5432/postgres', // Replace with your actual backend base URL
+        // You can add more default settings here
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:3000'
+          // Include other headers as needed, like Authorization for JWT
+        },
+      });
+
 
 }
 
--- 5leaguefantasyfrontend/src/components/league/LeagueInfo.js
+++ 5leaguefantasyfrontend/src/components/league/LeagueInfo.js
@@ -9,6 +9,32 @@ function LeagueInfo(){
         const storedData = localStorage.getItem("userData");
         return storedData ? JSON.parse(storedData) : null; // Set to null if userData is missing
     });
+
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate(); // For redirecting the user
+
+    useEffect(() => {
+        // Check if formData is null (which means userData was missing or default data would have been used)
+        if (!formData) {
+            // Simulate throwing a 401 unauthorized error
+            setError('Unauthorized access. Default data cannot be used.');
+            // Optionally, redirect the user to a login page or show an error message
+            navigate('/'); // Change '/login' to your actual login route
+        }
+    }, [formData, navigate]);
+
+    const handleEdit = () => setIsEditing(true);
+
+    const handleSave = () => setIsEditing(false);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
 }
 
 export default LeagueInfo;
