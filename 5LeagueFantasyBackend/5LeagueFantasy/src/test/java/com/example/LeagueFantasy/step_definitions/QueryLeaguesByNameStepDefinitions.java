39c88a7264f15aad3b9cf094d3162527d4d504ee
YassineMimet1 <yassine.mimet@mail.mcgill.ca>
2024-04-01 16:07:29 -0400

    added query Leagues By Name for join_league_by_name story

--- 5LeagueFantasyBackend/5LeagueFantasy/src/test/java/com/example/LeagueFantasy/step_definitions/QueryLeaguesByNameStepDefinitions.java
+++ 5LeagueFantasyBackend/5LeagueFantasy/src/test/java/com/example/LeagueFantasy/step_definitions/QueryLeaguesByNameStepDefinitions.java
@@ -1,10 +1,13 @@
 package com.example.LeagueFantasy.step_definitions;
 
 import com.example.LeagueFantasy.controller.LeagueController;
+import com.example.LeagueFantasy.dto.LeagueRequestDto;
 import com.example.LeagueFantasy.dto.LeagueResponseDto;
 import com.example.LeagueFantasy.entity.FantasyManager;
 import com.example.LeagueFantasy.entity.League;
+import com.example.LeagueFantasy.repository.FantasyManagerRepository;
 import com.example.LeagueFantasy.repository.LeagueRepository;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,6 +15,9 @@ import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
 
@@ -23,14 +29,33 @@ public class QueryLeaguesByNameStepDefinitions {
     @Autowired
     private LeagueRepository leagueRepository;
 
+    @Autowired
+    private FantasyManagerRepository fantasyManagerRepository;
+
     private List<LeagueResponseDto> queriedLeagues;
 
-    @Given("that there is a league name {string} in the system")
+    private FantasyManager mockManager;
+
+
+
+    @Given("the user is authenticated and on the Retrieve League page")
+    public void the_user_is_authenticated_and_on_the_create_league_page() {
+        mockManager = new FantasyManager();
+        mockManager.setUsername("username");
+        mockManager.setPassword("password");
+        mockManager.setEmail("email@example.com");
+        mockManager.setName("User's name");
+        // ... set other required fields ...
+        fantasyManagerRepository.save(mockManager);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(mockManager.getUsername(), mockManager.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    @And("that there is a league name {string} in the system")
     public void givenLeaguesExist(String leagueName){
-        League league = new League();
-        league.setName(leagueName);
-        league.setLeagueOwner(new FantasyManager());
-        leagueRepository.save(league);
+        LeagueRequestDto leagueRequestDto = new LeagueRequestDto();
+        leagueRequestDto.setName(leagueName);
+        leagueController.createLeague(leagueRequestDto);
     }
 
     @When("a user requests to retrieve leagues by name {string}")
--- 5LeagueFantasyBackend/5LeagueFantasy/src/test/resources/query_leagues_by_name.feature
+++ 5LeagueFantasyBackend/5LeagueFantasy/src/test/resources/query_leagues_by_name.feature
@@ -1,7 +1,8 @@
 Feature: Retrieving Leagues by Name
 
   Scenario Outline: Retrieving leagues by name
-    Given that there is a league name "<name>" in the system
+    Given the user is authenticated and on the Retrieve League page
+    And that there is a league name "<name>" in the system
     When a user requests to retrieve leagues by name "<name>"
     Then the system should return a list of leagues with the given "<name>"
 
